5572163bd7c462a419d7dd9ce0299f50c1a18a30
elisiomualumene <elisiomualumene@gmail.com>
2023-12-03 20:39:09 +0100

    feat: create entity.ts

---
+++ core/entity/entity.ts
@@ -0,0 +1,15 @@
+import { UniqueEntityId } from "./UniqueEntityId";
+
+export class Entity<T> {
+  private _id: UniqueEntityId;
+  protected props: T;
+
+  get id() {
+    return this._id;
+  }
+
+  protected constructor(props: T, id?: UniqueEntityId) {
+    this.props = props;
+    this._id = id ?? new UniqueEntityId();
+  }
+}
