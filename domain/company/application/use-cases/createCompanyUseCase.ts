c05af61f125eb455b9ee317474580a50284adfea
elisiomualumene <elisiomualumene@gmail.com>
2023-12-03 20:39:25 +0100

    feat: create createCompanyUseCase.ts

---
+++ domain/company/application/use-cases/createCompanyUseCase.ts
@@ -0,0 +1,28 @@
+import { Either, left, right } from "../../../../core/either";
+import { CompanyNotFoundError } from "../../../../core/errors/use-cases-errors/companyNotFound";
+import { Company } from "../../enterprise/entities/company.entity";
+import { ICompanyRepository } from "../repositories/ICompanyRepository";
+
+type ICreateCompanyDTO = {
+  location: string;
+  name: string;
+};
+
+type CreateCompanyResponse = Either<CompanyNotFoundError, {}>;
+
+export class CreateCompanyUseCase {
+  constructor(private companyRepository: ICompanyRepository) {}
+
+  async execute(data: ICreateCompanyDTO): Promise<CreateCompanyResponse> {
+    const company = Company.create(data);
+    const companyAlreadyExists = await this.companyRepository.findById(
+      company.id.toValue()
+    );
+
+    if (companyAlreadyExists) {
+      return left(new CompanyNotFoundError());
+    }
+
+    return right({});
+  }
+}
